476b1d76a3a8ca8977a9bbab02555db05a52b083
Ray <tn710617@gmail.com>
2019-01-22 11:08:59 +0800

    Revise email type from uni to nullable

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -18,7 +18,7 @@ class CreateUsersTable extends Migration
             $table->string('name');
             $table->string('FB_id')->unique();
             $table->integer('phone_id')->nullable();
-            $table->string('email')->unique();
+            $table->string('email')->nullable();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password')->nullable();
             $table->integer('channel_id')->default(0);
