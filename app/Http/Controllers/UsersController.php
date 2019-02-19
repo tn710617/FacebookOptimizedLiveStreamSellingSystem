5f7557d29432360f7ac312c6a65719accb40e89d
Ray <tn710617@gmail.com>
2019-02-19 13:55:43 +0800

    [Revise] In get-user-info function, when FB_email is null, show the email recorded in database

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -78,7 +78,7 @@ class UsersController extends Controller {
     {
         $me = Helpers::getFacebookResources($request->bearerToken());
         $response = ['name'    => $me->getName(),
-                     'email'   => $me->getEmail(),
+                     'email'   => $me->getEmail() ?? User::getUser($request)->email,
                      'avatar'  => 'https://graph.facebook.com/' . User::getUser($request)->FB_id . '/picture?type=large',
                      'user_id' => User::getUserID($request),
                      'phone'   => 'NULL'
