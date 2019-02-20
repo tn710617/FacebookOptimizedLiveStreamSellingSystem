9be1c7ea6bccd5cc8aabf58cf7f0095c75a8dbb2
Ray <tn710617@gmail.com>
2019-02-20 20:15:34 +0800

    [Function] Added third party payment service table and data into migration as default

--- database/migrations/2019_02_01_130809_create_third_party_payment_services_table.php
+++ database/migrations/2019_02_01_130809_create_third_party_payment_services_table.php
@@ -1,5 +1,6 @@
 <?php
 
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
@@ -18,6 +19,10 @@ class CreateThirdPartyPaymentServicesTable extends Migration
             $table->string('name');
             $table->timestamps();
         });
+        DB::statement('INSERT INTO `third_party_payment_services` (`id`, `name`, `created_at`, `updated_at`)
+VALUES
+	(1,\'AllPay\',\'2019-02-20 06:50:18\',\'2019-02-20 06:50:18\'),
+	(2,\'PayPal\',\'2019-02-20 06:50:31\',\'2019-02-20 06:50:31\');');
     }
 
     /**
